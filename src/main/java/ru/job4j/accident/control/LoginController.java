fbdd5cad1e2e6d90d9d3eba16119130ab84cc57e
Windows <ivinjc@gmail.com>
2022-08-18 12:07:40 +0300

    Загрузка данных в вид [#295782 #330194]

--- src/main/java/ru/job4j/accident/control/LoginController.java
+++ src/main/java/ru/job4j/accident/control/LoginController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-
     @GetMapping("/login")
     public String loginPage(@RequestParam(value = "error", required = false) String error,
                             @RequestParam(value = "logout", required = false) String logout,
